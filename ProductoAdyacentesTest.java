712a2f1f661c1552fbc560c07f79340301ef8f3f
Antix199 <142464955+Antix199@users.noreply.github.com>
2023-09-22 22:14:10 -0300

    Update ProductoAdyacentesTest.java

--- ProductoAdyacentesTest.java
+++ ProductoAdyacentesTest.java
@@ -5,36 +5,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductoAdyacentesTest {
 
     @Test
-    public void testProductoAdyacenteValoresNegativos() {
+    public void testProductoAdyacentesValoresNegativos() {
         int[] arreglo = {-2, -3, -5, -7, -11};
         int resultadoEsperado = 77;
-        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacente(arreglo));
+        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacentes(arreglo));
     }
     @Test
-    public void testProductoAdyacenteMezclaPositivosNegativos() {
+    public void testProductoAdyacentesMezclaPositivosNegativos() {
         int[] arreglo = {-2, 3, -5, 7, -11};
         int resultadoEsperado = -6;
-        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacente(arreglo));
+        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacentes(arreglo));
     }
 
     @Test
-    public void testProductoAdyacenteMultiplesPares() {
+    public void testProductoAdyacentesMultiplesPares() {
         int[] arreglo = {2, 5, 2, 5, 2, 5};
         int resultadoEsperado = 10;
-        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacente(arreglo));
+        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacentes(arreglo));
     }
     @Test
-    public void testProductoAdyacenteConCeros() {
+    public void testProductoAdyacentesConCeros() {
         int[] arreglo = {0, 0, 0, 0, 0, 0};
         int resultadoEsperado = 0; // 7 * 8
-        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacente(arreglo));
+        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacentes(arreglo));
     }
 
     @Test
-    public void testProductoAdyacenteRepetidos() {
+    public void testProductoAdyacentesRepetidos() {
         int[] arreglo = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,};
         int resultadoEsperado = 81; // 7 * 8
-        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacente(arreglo));
+        assertEquals(resultadoEsperado, ProductoAdyacentes.productoAdyacentes(arreglo));
     }
 
 }
